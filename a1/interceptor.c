64f3601c7711c564807d94c8caadd6e9369b3233
peizhizhang <peizhi.zhang@mail.utoronto.ca>
2019-01-18 19:36:58 -0500

    e

--- a1/interceptor.c
+++ a1/interceptor.c
@@ -294,7 +294,7 @@ asmlinkage long interceptor(struct pt_regs reg) {
 			}
 		} else {
       // all are monitored
-			log_message(current->pid, sysc, reg.bx, reg.cx, reg.dx, reg.si, reg.edi, reg.ebp);
+			log_message(current->pid, sysc, reg.bx, reg.cx, reg.dx, reg.si, reg.di, reg.bp);
 		}
 	} else {
 		// Not Intercepted
